1d07b9a5613646a43d05a84d497485dea8a90b15
Marek Malik(mm's) <info@code-house.pl>
2020-08-22 21:17:52 +0200

    facebook - change request for data delation

--- src/main/java/pl/code/house/makro/mapa/auth/domain/management/UserOptOutResource.java
+++ src/main/java/pl/code/house/makro/mapa/auth/domain/management/UserOptOutResource.java
@@ -3,12 +3,12 @@ package pl.code.house.makro.mapa.auth.domain.management;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static pl.code.house.makro.mapa.auth.ApiConstraints.EXTERNAL_AUTH_BASE_PATH;
 
+import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.code.house.makro.mapa.auth.domain.management.dto.UserDataResponseDto;
 
@@ -19,8 +19,8 @@ import pl.code.house.makro.mapa.auth.domain.management.dto.UserDataResponseDto;
 class UserOptOutResource {
 
   @PostMapping(path = "/facebook/opt-out")
-  ResponseEntity<UserDataResponseDto> userOptOut(@RequestParam("signedRequest") String signedRequest) {
-    log.info("Registered Facebook User OptOut call. signedRequest: `{}`", signedRequest);
+  ResponseEntity<UserDataResponseDto> userOptOut(Map<String, Object> request) {
+    log.info("Registered Facebook User OptOut call. signedRequest: `{}`", request);
 
     return ResponseEntity.ok(new UserDataResponseDto("https://www.makromapa.pl/", "200"));
   }
