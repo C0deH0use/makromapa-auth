2ff32081616776b90f6beb5e97bbf94fdf0d10a1
Marek Malik(mm's) <info@code-house.pl>
2020-08-26 23:02:56 +0200

    improve access token expiry time

--- connect-to-gcloud-test-db.sh
+++ connect-to-gcloud-test-db.sh
@@ -10,4 +10,4 @@
 # ➜ /usr/local/Cellar/postgresql/12.3_4/bin/psql --file=/Users/mmalik/Documents/WORK/MakroMapa/data_dump_2020_03.sql --username=makromapa-db-user --host=127.0.0.1 --port=5432 makromapa-db-prod
 ###########
 
-./cloud_sql_proxy -instances="makromapa-dev-1:europe-west1:makromapa-auth-prod=tcp:5432"
+./cloud_sql_proxy -instances="makromapa-dev-1:europe-west1:makromapa-auth=tcp:5432"
