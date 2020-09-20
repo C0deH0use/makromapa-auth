68863dd28a8f80b69d5d8405091f17b772134e65
Marek00Malik <info@code-house.pl>
2020-09-20 23:34:17 +0200

    registration and password reset e-mails update

--- src/integrationTest/java/pl/code/house/makro/mapa/auth/domain/mail/MaintenanceEmailServiceHttpTest.java
+++ src/integrationTest/java/pl/code/house/makro/mapa/auth/domain/mail/MaintenanceEmailServiceHttpTest.java
@@ -1,27 +1,34 @@
 package pl.code.house.makro.mapa.auth.domain.mail;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomNumeric;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.thymeleaf.context.Context;
 import pl.code.house.makro.mapa.auth.domain.mail.dto.RegistrationMessageDetails;
 import pl.code.house.makro.mapa.auth.domain.mail.dto.ResetPasswordMessageDetails;
 
 @SpringBootTest
 @Tag("maintenance")
+@TestPropertySource(properties = {
+    "spring.mail.port=465",
+    "spring.mail.host=ssl0.ovh.net",
+    "spring.mail.username=kontakt@makromapa.pl",
+    "spring.mail.protocol=smtps",
+    "spring.mail.password=****",
+})
 class MaintenanceEmailServiceHttpTest {
 
-  public static final String EXPECTED_SUBJECT = "REGISTRATION_SUBJECT";
-  public static final String EXPECTED_RESET_PASSWORD_SUBJECT = "RESET_PASSWORD_SUBJECT";
-  public static final String ACTIVATION_CODE = randomNumeric(6);
-  public static final String EXPIRY_DATE = "2020-09-15 08:03:48";
-//    public static final String EXPECTED_RECEIVER = "aga.poczta3@autograf.pl";
-  public static final String EXPECTED_RECEIVER = "Marek00Malik@gmail.com";
+  private static final String EXPECTED_SUBJECT = "REGISTRATION_SUBJECT";
+  private static final String EXPECTED_RESET_PASSWORD_SUBJECT = "RESET_PASSWORD_SUBJECT";
+  private static final String ACTIVATION_CODE = randomNumeric(6);
+  private static final String EXPIRY_DATE = "2020-09-15 08:03:48";
+  private static final String EXPECTED_RECEIVER = "aga.ebox@gmail.com";
+//  private static final String EXPECTED_RECEIVER = "Marek00Malik@gmail.com";
 
   @Autowired
   private EmailService sut;
