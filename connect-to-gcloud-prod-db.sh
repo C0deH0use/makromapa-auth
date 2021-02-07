bdfcd4dc83477657eff4b57f494fa8f9a9706090
mmalik <Marek.Malik@itds.pl>
2021-02-07 14:05:29 +0100

    upgrade Gradle Wrapper

--- connect-to-gcloud-prod-db.sh
+++ connect-to-gcloud-prod-db.sh
@@ -3,12 +3,12 @@
 ## When connecting to the db with postgres client you need to select the host as localhost !
 
 ### DUMP DATA FROM PROD
-# ➜ /usr/local/Cellar/libpq/13.1/bin/pg_dump --dbname=makromapa-auth-db --file=/Users/mmalik/Documents/MakroMapa/auth_dump_12_12_2020.sql --schema=public --username=makromapa-auth-user --host=127.0.0.1 --port=5432
+# ➜ /usr/local/Cellar/libpq/13.1/bin/pg_dump --dbname=makromapa-auth --file=/Users/mmalik/Documents/MakroMapa/auth/07_02_2021.sql --schema=public --username=makromapa-auth-user --host=127.0.0.1 --port=5432
 
 ###########
 
 ### RESTORE DATA FROM DATA DUMP FILE
-# ➜ /usr/local/Cellar/libpq/13.1/bin/psql --file=/Users/mmalik/Documents/MakroMapa/auth_dump_12_12_2020.sql --username=makromapa-auth-user --host=127.0.0.1 --port=5432 makromapa-auth
+# ➜ /usr/local/Cellar/libpq/13.1/bin/psql --file=/Users/mmalik/Documents/MakroMapa/auth/07_02_2021.sql --username=makromapa-auth-user --host=127.0.0.1 --port=5432 makromapa-auth
 ###########
 
 ./cloud_sql_proxy -instances="makromapa-297422:europe-west1:makromapa-auth-db=tcp:5432"
